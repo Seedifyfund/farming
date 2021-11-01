e71fd388f041a46dd18bcfdf34be6ca563c58b79
ChaitanyaKReddy <82520449+ChaitanyaKReddy@users.noreply.github.com>
2021-11-01 13:08:57 +0530

    Updated fetchUserShare

--- contracts/LockedFarming.sol
+++ contracts/LockedFarming.sol
@@ -432,7 +432,7 @@ contract SMD_v5 is Ownable {
         );
         uint256 userAmount = deposits[from].amount;
         require(userAmount > 0, "No stakes available for user"); //extra check
-        return (userAmount.mul(10 ** 54).div(stakedBalance)); //returns percentage upto 2 decimals
+        return 1;
     }
 
     function claimRewards() public returns (bool) {

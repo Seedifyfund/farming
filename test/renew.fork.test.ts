049251768526b59a586af0815765da603b2d9c53
Théo RICHARD <mail@rcode.tech>
2023-11-02 17:34:04 +0100

    fix: use `BigNumber` mainnet details

--- test/renew.fork.test.ts
+++ test/renew.fork.test.ts
@@ -28,57 +28,90 @@ describe('simulating mainnet test transaction locally', () => {
     const deploymentTimestamp = 1698648273;
     const periodOne = {
         at: 1698692920,
-        rewardAmount: 15000000000000000000,
+        rewardAmount: ethers.BigNumber.from('15000000000000000000'),
         start: 1698694200,
         end: 1698753600,
         lockDuration: 1,
     };
     const periodTwo = {
         at: 1698756395,
-        rewardAmount: 4000000000000000000,
+        rewardAmount: ethers.BigNumber.from('4000000000000000000'),
         start: 1698756600,
         end: 1698764400,
         lockDuration: 1,
     };
     const periodThree = {
         at: 1698767536,
-        rewardAmount: 950000000000000000,
+        rewardAmount: ethers.BigNumber.from('950000000000000000'),
         start: 1698768000,
         end: 1698778800,
         lockDuration: 1,
     };
     const periodFour = {
         at: 1698780101,
-        rewardAmount: 5000000000000000000,
+        rewardAmount: ethers.BigNumber.from('5000000000000000000'),
         start: 1698780600,
         end: 1698791400,
         lockDuration: 1,
     };
 
     const periodOneUserAction = {
-        serhat: { stake: { amount: 19499439399175780, at: 1698695348 } },
+        serhat: {
+            stake: {
+                amount: ethers.BigNumber.from('19499439399175780'),
+                at: 1698695348,
+            },
+        },
     };
     const periodTwoUserAction = {
         serhat: {
-            renew: { rewardsAmount: 14710101009675194901, at: 1698760054 },
+            renew: {
+                rewardsAmount: ethers.BigNumber.from('14710101009675194901'),
+                at: 1698760054,
+            },
         },
-        bruno: { stake: { amount: 10000000000000000, at: 1698762839 } },
+        bruno: {
+            stake: {
+                amount: ethers.BigNumber.from('10000000000000000'),
+                at: 1698762839,
+            },
+        },
     };
     const periodThreeUserAction = {
         serhat: {
-            renew: { rewardsAmount: 0, at: 1698770699 },
-            claim: { received: 461771235767807234, at: 1698775949 },
+            renew: {
+                rewardsAmount: ethers.BigNumber.from('0'),
+                at: 1698770699,
+            },
+            claim: {
+                received: ethers.BigNumber.from('461771235767807234'),
+                at: 1698775949,
+            },
         },
         julia: {
-            stake: { amount: 1949943940539, at: 1698772050 },
-            claim: { received: 36316272683742, at: 1698776179 },
+            stake: {
+                amount: ethers.BigNumber.from('1949943940539'),
+                at: 1698772050,
+            },
+            claim: {
+                received: ethers.BigNumber.from('36316272683742'),
+                at: 1698776179,
+            },
         },
     };
     const periodFourUserAction = {
         serhat: {
-            renew: { rewardsAmount: 250757311330929363, at: 1698780820 },
+            renew: {
+                rewardsAmount: ethers.BigNumber.from('250757311330929363'),
+                at: 1698780820,
+            },
         },
-        julia: { stake: { amount: 1949943940539, at: 1698787130 } },
+        julia: {
+            stake: {
+                amount: ethers.BigNumber.from('1949943940539'),
+                at: 1698787130,
+            },
+        },
     };
 
     beforeEach(async () => {
